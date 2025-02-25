a4420714f429deb12652699de3a1c11d4edb6c4c
Mostapha Bourarach <femtonet.email@gmail.com>
2025-02-25 17:43:23 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page.component'),
+    children: [
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search/search.component'),
+      },
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/trending-page/trending-page.component'),
+      },
+      {
+        path: "**",
+        redirectTo: "trending",
+      }
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
